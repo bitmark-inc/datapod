1cc2d1fe5ebe057a8a2f3712aa0bace49126c76b
Aaron Chang <xesuschang@gmail.com>
2020-02-05 10:01:54 +0800

    add reaction orm

--- data-parser/schema/facebook/reaction.go
+++ data-parser/schema/facebook/reaction.go
@@ -1,8 +1,11 @@
 package facebook
 
 import (
+	"fmt"
 	"github.com/alecthomas/jsonschema"
+	"github.com/jinzhu/gorm"
 	"github.com/xeipuuv/gojsonschema"
+	"time"
 )
 
 type RawReaction struct {
@@ -37,4 +40,51 @@ func ReactionSchemaLoader() *gojsonschema.Schema {
 	schemaLoader := gojsonschema.NewStringLoader(string(data))
 	schema, _ := gojsonschema.NewSchema(schemaLoader)
 	return schema
+}
+
+type ReactionORM struct {
+	gorm.Model
+	ReactionID  int `gorm:"column:reaction_id"`
+	Timestamp   int
+	Date        string
+	Weekday     int
+	Title       string
+	Actor       string
+	Reaction    string
+	DataOwnerID string `gorm:"column:data_owner_id"`
+}
+
+func (r RawReaction) Write(db *gorm.DB) {
+	for _, reaction := range r.Reactions {
+		t := time.Unix(int64(reaction.Timestamp), 0)
+
+		title, err := reaction.Title.String()
+		if nil != err {
+			fmt.Printf("convert reaction title with error: %s", err)
+		}
+
+		tmp := reaction.Data[0]
+		actor, err := tmp.Actor.String()
+		if nil != err {
+			fmt.Printf("convert reaction actor with error: %s", err)
+		}
+
+		react, err := tmp.Actor.String()
+		if nil != err {
+			fmt.Printf("convert reaction with error: %s", err)
+		}
+
+		orm := ReactionORM{
+			ReactionID:  0, // TODO: reaction id
+			Timestamp:   reaction.Timestamp,
+			Date:        dateOfTime(t),
+			Weekday:     weekdayOfTime(t),
+			Title:       title,
+			Actor:       actor,
+			Reaction:    react,
+			DataOwnerID: "", // TODO: data owner id
+		}
+
+		// db.Create(orm) // TODO: batch update
+	}
 }
